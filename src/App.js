1ad47b191ba343eb955c97e48b5c800b152b9146
Noobra-dev <sayan98das98@gmail.com>
2023-08-25 21:47:18 +0530

    Multiselect Dropdown

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+import MultiSelectAll from "./MultiSelectAll";
+
+
+function App() {
+  return (
+    <div className="App">
+       <MultiSelectAll />
+    </div>
+  );
+}
+
+export default App;
